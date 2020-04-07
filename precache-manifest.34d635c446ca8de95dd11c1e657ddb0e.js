ead4bb19a6d3c36540e20e3ad78c45079eaf050c
ankiiitraj <ankitatiiitr@gmail.com>
2020-04-08 01:56:42 +0530

    Updates

---
+++ precache-manifest.34d635c446ca8de95dd11c1e657ddb0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d33979d50795a9b52d7e8de90052bf",
+    "url": "/naya/index.html"
+  },
+  {
+    "revision": "d6d2b359b7055178c065",
+    "url": "/naya/static/css/main.71850710.chunk.css"
+  },
+  {
+    "revision": "7f647066ef2cfbde46c5",
+    "url": "/naya/static/js/2.230d5876.chunk.js"
+  },
+  {
+    "revision": "99740f07e59da1777ca51e4cdac8fee9",
+    "url": "/naya/static/js/2.230d5876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d2b359b7055178c065",
+    "url": "/naya/static/js/main.08a5bc6f.chunk.js"
+  },
+  {
+    "revision": "026f2274bd070f6386ad",
+    "url": "/naya/static/js/runtime-main.1040eca9.js"
+  },
+  {
+    "revision": "ddbdf10c89efd2c2202513ab0ee0c458",
+    "url": "/naya/static/media/360.ddbdf10c.png"
+  }
+]);
