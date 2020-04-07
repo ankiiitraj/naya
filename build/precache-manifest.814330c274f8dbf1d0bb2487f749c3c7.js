7d3bcbd54604c2e1f6e05079ecd257df046dd3bd
ankiiitraj <ankitatiiitr@gmail.com>
2020-04-08 01:45:25 +0530

    pushing production

---
+++ build/precache-manifest.814330c274f8dbf1d0bb2487f749c3c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ab9f2b830f0ee4d50c2070d477ff25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7af8586e7c184061fd2",
+    "url": "/static/css/main.71850710.chunk.css"
+  },
+  {
+    "revision": "7f647066ef2cfbde46c5",
+    "url": "/static/js/2.230d5876.chunk.js"
+  },
+  {
+    "revision": "99740f07e59da1777ca51e4cdac8fee9",
+    "url": "/static/js/2.230d5876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7af8586e7c184061fd2",
+    "url": "/static/js/main.cc044a50.chunk.js"
+  },
+  {
+    "revision": "01b848a486c75eece0c8",
+    "url": "/static/js/runtime-main.851aca57.js"
+  },
+  {
+    "revision": "ddbdf10c89efd2c2202513ab0ee0c458",
+    "url": "/static/media/360.ddbdf10c.png"
+  }
+]);
